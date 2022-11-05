250beffc33c55f507aa69be0e94988725e9b4d08
Dubois Hugo <195347@ecam.be>
2022-11-05 14:19:40 +0100

    #2 Get JSON from the topic

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const host = 'eu1.cloud.thethings.network'
 const port = '1883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 const connectUrl = `mqtt://${host}:${port}`
+var fs = require('fs');
+let Data = [];
 
 const client = mqtt.connect(connectUrl, {
     clientId,
@@ -14,19 +16,29 @@ const client = mqtt.connect(connectUrl, {
 })
 
 client.on('connect', () => {
-  var topic = "v3/ecam-dht11@ttn/devices/eui-70b3d57ed0055dbb/up";console.log('Connected')
+  var topic = "v3/room-quality@ttn/devices/device-1/up";
+  console.log('Connected')
   client.subscribe(topic); //single topic
   console.log("connected +subscribed");
 });
 
+// Getting the data from a device with MQTT protocol
 client.on("message", function (topic, message, packet) {
   var getDataFromTTN = JSON.parse(message);
+  device_id = getDataFromTTN.end_device_ids.device_id;
   data = getDataFromTTN.uplink_message.decoded_payload;
-  console.log("message is " + message);
-  console.log("topic is " + topic);
+  let data_device = {"device_id": device_id, "data" : data}
+  Data.push(data_device);
+  var Data_file = JSON.stringify(Data, null, 2);
+  fs.writeFile("Data_By_Device.json", Data_file, function(err, result) {
+    if(err) console.log('error', err);
+  });
 });
+
+
 
 client.on("error", function (error) {
   console.log("Can't connect" + error);
   process.exit(1)
 });
+
